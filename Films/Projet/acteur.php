80a46a2b0415358b28bf74d52228bef14566f15b
JustJerem <guillot.jeremie@gmail.com>
2016-04-11 20:52:41 +0200

    Inscription dans la base de donnée en POO d'un acteur

---
+++ Films/Projet/acteur.php
@@ -0,0 +1,85 @@
+<?php
+include("connexion.php");
+
+
+class Acteur
+{
+
+    /****************/
+    /*    METHODE   */
+    /***************/
+
+    public function ajout($nom_acteur, $prenom_acteur)
+    {
+
+        if (!empty($nom_acteur) || !empty($prenom_acteur)) {
+            $infoActeurTableau = array('n' => $nom_acteur, 'p' => $prenom_acteur);
+            $bdd = connectDb();
+            $query = $bdd->prepare('SELECT COUNT(*) AS nb FROM acteur WHERE NOM_ACTEUR = :n && PRENOM_ACTEUR = :p');
+            $query->execute($infoActeurTableau);
+            $data = $query->fetch();
+
+            if ($data['nb'] >= 1) {
+
+                echo 'L"acteur '.$prenom_acteur.' '.$nom_acteur.' existe déjà ';
+
+            } else {
+
+                //Ajout du film
+                $bdd = connectDb();
+                $query = $bdd->prepare('INSERT INTO `acteur`(`NOM_ACTEUR`, `PRENOM_ACTEUR`) VALUES (:n, :p)');
+                $query->execute($infoActeurTableau);
+
+                echo 'L"acteur '.$prenom_acteur.' '.$nom_acteur.' a été inséré.';
+
+            }
+
+        }
+    }
+
+    /****************/
+    /*GETTER SETTTER*/
+    /***************/
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNomActeur()
+    {
+        return $this->nom_acteur;
+    }
+
+    public function getPrenomActeur()
+    {
+        return $this->prenom_acteur;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setNomActeur($nom_acteur)
+    {
+        $this->nom_acteur = $nom_acteur;
+    }
+
+    public function setPrenomActeur($prenom_acteur)
+    {
+        $this->prenom_acteur = $prenom_acteur;
+    }
+
+    /****************/
+    /*  ATTRIBUTS  */
+    /***************/
+
+    public $id;
+    public $nom_acteur;
+    public $prenom_acteur;
+
+
+}
+
+?>
---
+++ Films/Projet/insertionActeur.php
@@ -0,0 +1,57 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Inséré</title>
+    <?php include("acteur.php"); ?>
+</head>
+<body>
+Insertion dans la base de donnée.
+
+<?php
+$nom_acteur = $_POST['nom_acteur'];
+$prenom_acteur = $_POST['prenom_acteur'];
+$monActeur = new Acteur();
+$monActeur->ajout($nom_acteur, $prenom_acteur);
+?>
+<?php
+//		$bdd=connectDb();
+//
+//
+//		$query=$bdd->prepare('SELECT COUNT(*) AS nb FROM films WHERE nom_film = :n');
+//		$query->execute(array('n'=> $_POST['nom_film']));
+//		$data = $query->fetch();
+//
+//
+//		//Création du tableau
+//		$tableauValeur=array('n'=> $_POST['nom_film'],'a'=> $_POST['annee_prod'],'s'=> $_POST['score']);
+//		//var_dump ($data['nb']);
+//		//var_dump ($tableauValeur);
+//
+//
+//
+//		// Update du film
+//		if  ($data['nb'] >= 1){
+//			$bdd=connectDb();
+//			$query=$bdd->prepare('UPDATE `films` SET `annee_film`=:a,`score`=:s WHERE `nom_film`=:n');
+//			$query->execute($tableauValeur);
+//
+//			echo 'Le film a été mis à jour.';
+//
+//		}
+//		else{
+//
+//		//Ajout du film
+//        $bdd=connectDb();
+//		$query=$bdd->prepare('INSERT INTO `films`(`nom_film`, `annee_film`, `score`) VALUES (:n, :a, :s)');
+//		$query->execute($tableauValeur);
+//
+//			echo 'L"acteur  a été inséré.';
+//
+//		}
+//
+//
+?>
+
+</body>
+</html>
