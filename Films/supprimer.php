2a14e9c72f69b6d53f1e8c57bd457e9e6e665c47
fanny-r <fanny.rottee@gmail.com>
2016-04-24 18:24:38 +0200

    Rend valide W3c

--- Films/supprimer.php
+++ Films/supprimer.php
@@ -12,7 +12,7 @@
 <!--Suppression d'un film-->
 <div class="container">
     <h2>Selectionner l'acteur <strong>ou</strong> le film à supprimer</h2>
-    <form role="form" method="post" action="supprimer_film.php">
+    <form method="post" action="supprimer_film.php">
         <div class="form-group">
             <label for="sel1">Sélectionner un film:</label>
             <select name="Film" size="1" class="form-control" id="sel1">
@@ -35,10 +35,10 @@
 
 <!--Suppression d'un acteur-->
 <div class="container">
-    <form role="form" method="post" action="supprimer_acteur.php">
+    <form method="post" action="supprimer_acteur.php">
         <div class="form-group">
-            <label for="sel1">Sélectioner un acteur:</label>
-            <select name="Acteur" size="1" class="form-control" id="sel1">
+            <label for="sel2">Sélectioner un acteur:</label>
+            <select name="Acteur" size="1" class="form-control" id="sel2">
 
                 <?php
                 $data = $repoActeur->getAllActeur();
